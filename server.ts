7aa2ba46d83b01e1215ac3473dd9a9435001f8a2
Lennon Day-Reynolds <lennon@rcoder.net>
2019-10-13 05:05:32 -0700

    now committing all changes to git...what could possibly go wrong?

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ import cors from 'fastify-cors';
 
 import nedb from 'nedb-promises';
 import minimist from 'minimist';
+import { Option } from 'prelude-ts';
+
+import * as git from 'isomorphic-git';
 
 import fs from 'fs';
 import path from 'path';
@@ -17,20 +20,14 @@ if (argv._.length == 0) {
 const isDirectory = (path: string) => 
   fs.existsSync(path) && fs.statSync(path).isDirectory();
 
-
 const config = JSON.parse(fs.readFileSync(argv._[0]).toString());
 const dataDir = config.data;
 
+const gitAuthor = config.git.author;
+
 if (!dataDir || !isDirectory(dataDir)) {
   fs.mkdirSync(dataDir);
-  //throw new Error(`data directory ${dataDir} does not exist, or is not a directory`);
 }
-
-const openDb = (name: string) => nedb.create({
-  filename: path.join(dataDir, path.basename(name)),
-  autoload: true,
-  timestampData: true
-});
 
 const server = fastify({
   logger: { level: config.logLevel || 'info' }
@@ -41,6 +38,61 @@ server.register(sensible);
 server.register(cors, {
   origin: config.origin || '*'
 });
+
+git.plugins.set('fs', fs);
+const gitDir = path.join(dataDir, '.git');
+
+if (!fs.existsSync(gitDir)) {
+  git.init({
+    dir: dataDir
+  }).then(() =>
+    server.log.info(`initialized git repo at ${dataDir}/.git`)
+  );
+}
+
+const dbCache: Map<string, nedb> = new Map();
+
+const openDb = (name: string) => {
+  if (dbCache.has(name)) {
+    return dbCache.get(name);
+  }
+
+  const basename = path.basename(name);
+  const fullPath = path.join(dataDir, basename);
+  
+  const db = nedb.create({
+    filename: fullPath,
+    autoload: true,
+    timestampData: true
+  });
+
+  fs.watchFile(fullPath, { persistent: false }, (curr, prev) => {
+    if (curr.mtime > prev.mtime) {
+      git.add({ dir: dataDir, filepath: basename });
+    }
+  })
+
+  dbCache.set(name, db);
+  return db;
+};
+
+process.on('SIGUSR2', async e =>
+  Promise.all(Array.from(dbCache.keys()).map(async name => {
+    const db = dbCache.get(name);
+
+    if (db) {
+      // okay, sometimes the type checker really isn't doing us any favors...like when
+      // the typings have a big hole named 'persistence'
+      Option.of((db as any).persistence).map(async (p: any) => await p.compactDatafile());
+    }
+
+    return await git.add({ dir: dataDir, filepath: path.basename(name) })
+  })).then(async () => await git.commit({
+    dir: dataDir,
+    message: 'automatic commit based on signal',
+    author: gitAuthor
+  }))
+);
 
 if (config.keys) {
   server.register(
@@ -54,6 +106,9 @@ const paginationQuerySchema = {
   limit: { type: 'number' }
 }
 
+const tryOpenDb = async (name: string | undefined) =>
+  await openDb((Option.of(name).getOrThrow())) as nedb;
+
 server.route({
   url: '/db/:name',
   method: 'GET',
@@ -61,14 +116,14 @@ server.route({
     querystring: paginationQuerySchema
   },
   handler: async (req, reply) => 
-    reply.send(await openDb(req.params.name).find(req.body))
+    reply.send((await tryOpenDb(req.params.name)).find(req.body))
 });
 
 server.route({
   url: '/db/:name',
   method: 'POST',
   handler: async (req, reply) =>
-    reply.send(await openDb(req.params.name).insert(req.body))
+    reply.send((await tryOpenDb(req.params.name)).insert(req.body))
 });
 
 server.route({
@@ -86,8 +141,7 @@ server.route({
     }
   },
   handler: async (req, reply) => 
-    reply.send(await
-      openDb(req.params.name).update(req.body.where, req.body.with)
+    reply.send((await tryOpenDb(req.params.name)).update(req.body.where, req.body.with)
     )
 });
 
@@ -95,9 +149,7 @@ server.route({
   url: '/db/:name',
   method: 'DELETE',
   handler: async (req, reply) => 
-    reply.send(await
-      openDb(req.params.name).remove(req.body, {multi: true})
-    )
+    reply.send((await tryOpenDb(req.params.name)).remove(req.body, {multi: true}))
 });
 
 server.listen(config.port || 9080);
